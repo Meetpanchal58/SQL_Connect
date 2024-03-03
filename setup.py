b6c94d151bb7b3c3b5fbfe78c31b0bd8a76a6c2a
meetpanchal58 <meetpanchal0580@gmail.com>
2024-03-04 03:00:14 +0530

    first release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()  
 
 
-__version__ = "0.0.1"
+__version__ = "0.01"
 REPO_NAME = "SQL_Connect"
 PKG_NAME= "load_MySQL"
 AUTHOR_USER_NAME = "meetpanchal58"
