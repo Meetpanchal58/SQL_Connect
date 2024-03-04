a79f572dc53cc35ef40615b8bf95627aad2c3703
meetpanchal58 <meetpanchal0580@gmail.com>
2024-03-04 19:42:19 +0530

    updated release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()  
 
 
-__version__ = "1.0"
+__version__ = "1.0.0"
 REPO_NAME = "SQL_Connect"
 PKG_NAME= "load_MySQL"
 AUTHOR_USER_NAME = "meetpanchal58"
@@ -29,7 +29,7 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with SQL Database and performing essential operations.",
+    description="A python package for connecting with sql database and performing essential operations.",
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
